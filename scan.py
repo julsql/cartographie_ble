5800f802ce8f23290ee3fc883e5150784ae5deda
Théo LardeurGersztein <theo.lardeur-gersztein@telecom-sudparis.eu>
2023-03-23 14:26:26 +0100

    Adresse MAC Théo

--- scan.py
+++ scan.py
@@ -9,5 +9,6 @@ async def main():
     async with aioble.scan(duration_ms=5000) as scanner:
         async for result in scanner:
             print(result, result.name(), result.rssi, result.services())
+            
 
 asyncio.run(main())
