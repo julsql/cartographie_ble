0e9cca319d109d6a7169242359ee3693ab14d3ef
Juliette DEBONO <juliette.debono@telecom-sudparis.eu>
2023-05-17 20:14:15 +0200

    ajout reponse server

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-import uasyncio as asyncio
-import aioble
+# Fichier led.py --> Allumer la led de la ESP32-S3
 
-print("Hello World")
+import machine
+import neopixel
+import time
 
-async def main():
-    async with aioble.scan(duration_ms=5000) as scanner:
-        async for result in scanner:
-            print(result, result.name(), result.rssi, result.services())
+print("Allumer la LED")
 
-asyncio.run(main())
+# Configuration de la broche de la bande de LEDs
+led_pin = machine.Pin(48, machine.Pin.OUT)
+
+# Configuration du nombre de LEDs dans la bande
+num_leds = 1
+
+# Création d'un objet "NeoPixel" avec la configuration de la bande de LEDs
+leds = neopixel.NeoPixel(led_pin, num_leds)
+
+# Activer la LED avec une couleur spécifique (ici, rouge)
+leds[0] = (255, 0, 0)
+leds.write()
+time.sleep(3)
+leds[0] = (0, 0, 0)
+leds.write()
--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 import uasyncio as asyncio
 import aioble
 import bluetooth
-
+import struct
+from micropython import const
 
 # Randomly generated UUIDs.
 _FILE_SERVICE_UUID = bluetooth.UUID("0492fcec-7194-11eb-9439-0242ac130002")
 _CONTROL_CHARACTERISTIC_UUID = bluetooth.UUID("0492fcec-7194-11eb-9439-0242ac130003")
+_COMMAND_SEND = const(0)
 
 # How frequently to send advertising beacons.
 _ADV_INTERVAL_MS = 250_000
@@ -16,6 +18,8 @@ control_characteristic = aioble.Characteristic(
     file_service, _CONTROL_CHARACTERISTIC_UUID, write=True, notify=True
 )
 aioble.register_services(file_service)
+
+server_mac = ""
 
 async def control_task(connection):
     try:
@@ -40,6 +44,38 @@ async def control_task(connection):
     except aioble.DeviceDisconnectedError:
         return
     
+async def write(connection, data):
+    try:
+        print("Sending data")
+        file_service = await connection.service(_FILE_SERVICE_UUID)
+        new_control_characteristic = await file_service.characteristic(
+            _CONTROL_CHARACTERISTIC_UUID
+        )
+        await new_control_characteristic.write(struct.pack("<BB", _COMMAND_SEND, 1) + data.encode())
+
+
+    except asyncio.TimeoutError:
+        print("Timeout discovering services/characteristics")
+        return
+    
+async def get_neighbour():
+    neigh = ""
+    async with aioble.scan(duration_ms=5000) as scanner:
+        async for result in scanner:
+            mac = get_mac_address(result.device.addr)
+            print("MAC ADDRESS: ", mac)
+            if mac != server_mac:
+                print(result, result.name(), result.rssi, result.services())
+                neigh += mac + ","
+    return neigh[:-1]
+    
+import binascii
+
+def get_mac_address(mac_bytes):
+    # result.device.addr
+    mac_string = binascii.hexlify(mac_bytes).decode('utf-8')
+    formatted_mac = ':'.join(mac_string[i:i+2].upper() for i in range(0, 12, 2))
+    return formatted_mac
 
 # Serially wait for connections. Don't advertise while a central is
 # connected.
@@ -49,10 +85,13 @@ async def peripheral_task():
         print("Waiting for connection")
         connection = await aioble.advertise(
             _ADV_INTERVAL_MS,
-            name="mpy-file",
+            name="esp-server",
             services=[_FILE_SERVICE_UUID],
         )
         print("Connection from", connection.device)
+        global server_mac
+        server_mac = get_mac_address(connection.device.addr)
+
 
         try:
             with connection.timeout(None):
@@ -62,16 +101,24 @@ async def peripheral_task():
                     msg = control_characteristic.read()
                     control_characteristic.write(b"")
 
+                    print(dir(control_characteristic))
+
                     if len(msg) < 3:
                         continue
 
                     # Message is <command><seq><path...>.
                     command = msg[0]
                     seq = msg[1]
-                    data = msg[2:]
+                    data = msg[2:].decode()
 
                     # Process the received data as needed
-                    print("Received data:", data.decode())
+                    print("Received data:", data)
+                    if data == "look":
+
+                        to_send = get_neighbour()
+
+                        await write(connection, to_send)
+
 
         except aioble.DeviceDisconnectedError:
             return
